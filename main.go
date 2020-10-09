25e4ade444e94353c133d9c5357527704d48a678
Stanford, Jon <jonathan.stanford@asurion.com>
2020-10-09 15:32:46 -0500

    Pin each certificate in the chain

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"crypto/tls"
 	"encoding/json"
 	"encoding/pem"
@@ -10,6 +11,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"reflect"
 	"time"
 )
 
@@ -19,18 +21,29 @@ func loggedCloser(c io.Closer) {
 	}
 }
 
-func getPemFromApi(domain string) (error, string) {
+func getPemFromApi(domain string) (error, []string) {
 	//goland:noinspection SpellCheckingInspection
+	type CertificateInChain struct {
+		CertificatePem string `json:"certificate_pem"`
+		Pem            struct {
+			Hashes struct {
+				Sha265 string `json:"sha256"`
+			} `json:"hashes"`
+		} `json:"pem"`
+	}
+
+	// only parse as little as possible
 	type CertistReply struct {
 		Certificate struct {
 			Pem string `json:"pem"`
 		} `json:"certificate"`
+		CertificatesInTheChain []CertificateInChain `json:"chain"`
 	}
 
 	var myClient = &http.Client{Timeout: 15 * time.Second}
 	r, err := myClient.Get(fmt.Sprintf("https://api.cert.ist/%s", domain))
 	if err != nil {
-		return err, ""
+		return err, []string{}
 	}
 	defer loggedCloser(r.Body)
 
@@ -39,44 +52,51 @@ func getPemFromApi(domain string) (error, string) {
 
 	err = json.Unmarshal(bod, &api)
 	if err != nil {
-		return err, ""
+		return err, []string{}
 	}
-	return nil, api.Certificate.Pem
+
+	all := make([]string, len(api.CertificatesInTheChain))
+	for c, cert := range api.CertificatesInTheChain {
+		all[c] = cert.Pem.Hashes.Sha265
+	}
+	return nil, all
 }
 
-func getPemFromLocal(address string) (error, string) {
+func getPemFromLocal(address string) (error, []string) {
 	conn, err := tls.Dial("tcp", address, &tls.Config{
 		InsecureSkipVerify: true,
 	})
 	if err != nil {
-		return err, ""
+		return err, []string{}
 	}
 	defer loggedCloser(conn)
-	var b bytes.Buffer
-	for _, cert := range conn.ConnectionState().PeerCertificates {
+	all := make([]string, len(conn.ConnectionState().PeerCertificates))
+	for count, cert := range conn.ConnectionState().PeerCertificates {
+		var b bytes.Buffer
 		err := pem.Encode(&b, &pem.Block{
 			Type:  "CERTIFICATE",
 			Bytes: cert.Raw,
 		})
 		if err != nil {
-			return err, ""
+			return err, []string{}
 		}
-		break
+		sum256 := sha256.Sum256([]byte(b.String()))
+		all[count] = fmt.Sprintf("%x", sum256)
 	}
-	return nil, b.String()
+	return nil, all
 }
 
 func validateCertificate(domain string) bool {
-	e, pemFoundLocally := getPemFromLocal(fmt.Sprintf("%s:443", domain))
+	e, hashesFromLocal := getPemFromLocal(fmt.Sprintf("%s:443", domain))
 	if e != nil {
 		panic(e)
 	}
-	e, pemFoundFromApi := getPemFromApi(domain)
+	e, hashesFromApi := getPemFromApi(domain)
 	if e != nil {
 		panic(e)
 	}
 
-	areEqual := pemFoundLocally == pemFoundFromApi
+	areEqual := reflect.DeepEqual(hashesFromLocal, hashesFromApi)
 	log.Printf("Domain: %s, Certificates matched? %t", domain, areEqual)
 	return areEqual
 }
